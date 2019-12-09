97c0675e4f907d1aea8db5f839b2465b1e13404f
Folkert van der Molen <f.van.der.molen@aspin.nl>
2019-12-09 16:00:37 +0100

    added all available properties for the WebhookSubscription endpoint

--- src/ExactOnline.Client.Models/Webhooks/WebhookSubscription.cs
+++ src/ExactOnline.Client.Models/Webhooks/WebhookSubscription.cs
@@ -8,6 +8,12 @@ namespace ExactOnline.Client.Models.Webhooks
     {
         public Guid ID { get; set; }
         public string CallbackURL { get; set; }
+        public Guid? ClientID { get; set; }
+        public DateTime? Created { get; set; }
+        public Guid? Creator { get; set; }
+        public string CreatorFullName { get; set; }
+        public string Description { get; set; }
+        public int? Division { get; set; }
         public string Topic { get; set; }
     }
 }
