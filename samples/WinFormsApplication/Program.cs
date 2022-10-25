1b895fa358b6f18e6bd208e9ed064facc021b3c4
fretje <github@fretje.be>
2022-10-25 17:47:05 +0200

    Update WebApplication to use OAuth2 and bring it inline with WebApplicationCore Rename winformsapplication to winformsapplicationcore Build OAuth2.WinForms for both dotnet core and framework Add winformsapplication .net framework which uses OAuth2.Winforms

--- samples/WinFormsApplication/Program.cs
+++ samples/WinFormsApplication/Program.cs
@@ -1,16 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
 namespace WinFormsApplication;
 
 internal static class Program
 {
-	/// <summary>
-	///  The main entry point for the application.
-	/// </summary>
-	[STAThread]
-	static void Main()
-	{
-		// To customize application configuration such as set high DPI settings or default font,
-		// see https://aka.ms/applicationconfiguration.
-		ApplicationConfiguration.Initialize();
-		Application.Run(new MainForm());
-	}
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
+    }
 }
--- samples/WinFormsApplicationCore/MainForm.cs
+++ samples/WinFormsApplicationCore/MainForm.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Windows.Forms;
-using ExactOnline.Client.Models.CRM;
+﻿using ExactOnline.Client.Models.CRM;
 using ExactOnline.Client.OAuth2.WinForms;
 using ExactOnline.Client.Sdk.Controllers;
 using ExactOnline.Client.Sdk.Test.Infrastructure;
@@ -10,15 +7,15 @@ namespace WinFormsApplication;
 
 public partial class MainForm : Form
 {
-    public MainForm()
-    {
-        InitializeComponent();
-    }
+	public MainForm()
+	{
+		InitializeComponent();
+	}
 
-    private async void MainForm_Load(object sender, EventArgs e)
-    {
+	private async void MainForm_Load(object sender, EventArgs e)
+	{
 		// To make this work set the authorisation properties of your test app in the testapp.config.
-		var testApp = new TestApp(@"..\..\..\..\testapp.config");
+		var testApp = new TestApp();
 
 		var authorizer = new ExactOnlineWinFormsAuthorizer(testApp.ClientId, testApp.ClientSecret, testApp.CallbackUrl, ExactOnlineTest.Url, ExactOnlineTest.AccessToken, ExactOnlineTest.RefreshToken, ExactOnlineTest.AccessTokenExpiresAt);
 		authorizer.TokensChanged += (sender, e) => (ExactOnlineTest.RefreshToken, ExactOnlineTest.AccessToken, ExactOnlineTest.AccessTokenExpiresAt) = (e.NewRefreshToken, e.NewAccessToken, e.NewExpiresAt);
@@ -31,6 +28,12 @@ public partial class MainForm : Form
 			// Get the Code and Name of a random account in the administration.
 			var fields = new[] { "Code", "Name" };
 			var account = (await client.For<Account>().Top(1).Select(fields).GetAsync()).List.FirstOrDefault();
+
+			// Get transaction lines for a specific entry number.
+			//var transactionlines = await client.For<TransactionLine>()
+			//	.Select(ModelInfo.For<TransactionLine>().FieldNames())
+			//	.Where(t => t.EntryNumber, 22010134, OperatorEnum.Eq)
+			//	.GetAsync();
 		}
 		catch (Exception ex)
 		{
